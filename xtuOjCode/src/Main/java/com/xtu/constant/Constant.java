65c7406a8cb53bc2acbd3a9bc4f76dfc1defbc06
PanDingrong0129 <1084288424@qq.com>
2017-04-12 21:31:12 +0800

    Add a Constant class

---
+++ xtuOjCode/src/Main/java/com/xtu/constant/Constant.java
@@ -0,0 +1,19 @@
+package com.xtu.constant;
+
+/**
+ * Created by Ilovezilian on 2017/4/12.
+ */
+public class Constant {
+    /**
+     * role is administrator
+     */
+    public static final int ADMIN = 0;
+    /**
+     * role is teacher
+     */
+    public static final int TEACHER = 1;
+    /**
+     * role is student
+     */
+    public static final int STUDENT = 2;
+}
